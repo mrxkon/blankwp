22f97a1edfd6b63074a6f5c08e741916625dfebc
mrxkon <mr.xkon@gmail.com>
2020-03-04 14:02:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,17 +21,17 @@
 	);
 	?>
 
-	<?php
-	if ( have_posts() ) {
-		while ( have_posts() ) {
-			the_post();
-		}
-	}
-	?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<h2><?php the_title(); ?></h2>
 
-	<?php the_content(); ?>
+	<div class="entry">
+		<?php the_content(); ?>
+	</div>
+
+	<?php endwhile; else : ?>
+		<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
 
 	<?php wp_footer(); ?>
 </body>
